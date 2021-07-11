4a5a4249bf328afcf49c098ce9c92f02dde9af24
Martin <marcin.paszkowski86@gmail.com>
2021-07-11 11:10:03 +0200

    add HackerRank solution - 1D Array Game

--- HackerRank/Game/src/main/java/Solution.java
+++ HackerRank/Game/src/main/java/Solution.java
@@ -2,31 +2,18 @@ import java.util.*;
 
 public class Solution {
 
+    private static boolean checkWay(int leap, int[] game, int pos){
+        if (pos<0 || (pos<game.length && game[pos]==1)) return false;
+        if (pos>= game.length || (pos== game.length-1 && game[pos]==0)) return true;
+
+        game[pos]=1;
+
+        return checkWay(leap, game, pos+1) || checkWay(leap, game, pos+leap) || checkWay(leap, game, pos-1);
+    }
+
     public static boolean canWin(int leap, int[] game) {
 
-        int playerIndex=0;
-        int block=0;
-        boolean tryJump = false;
-
-        while (playerIndex<game.length-1) {
-            if (!tryJump){
-                if (game[playerIndex+1]==0){
-                    playerIndex+=1;
-                }else {
-                    tryJump=true;
-                    block=playerIndex;
-                }
-            }else {
-                if (playerIndex+leap>game.length-1 || game[playerIndex+leap]==0){
-                    playerIndex+=leap;
-                    tryJump=false;
-                    if (playerIndex==block) return false;
-                } else if (playerIndex-1>=0 && game[playerIndex-1]==0){
-                    playerIndex-=1;
-                } else return false;
-            }
-        }
-        return true;
+        return checkWay(leap, game, 0);
     }
 
     public static void main(String[] args) {
