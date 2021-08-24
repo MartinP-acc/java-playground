d75d815396640708e1e3d8be5e03a67529d6b43d
Martin <marcin.paszkowski86@gmail.com>
2021-08-24 17:39:48 +0200

    add CodeWars kata solution - Snail Sort

---
+++ CodeWars/Kata/SnailSort/src/main/java/Snail.java
@@ -0,0 +1,35 @@
+public class Snail {
+
+    public static int[] snail(int[][] array) {
+        int len = array.length;
+        if (array[0].length==0) return new int[0];
+        int[] snailSorted = new int[len*len];
+        int row = 0;
+        int col = 0;
+        int direction = 0;
+        int counter = 0;
+        for (int i=0; i< snailSorted.length; i++){
+
+            snailSorted[i] = array[row][col];
+
+            if (counter==len-1) {
+                direction++;
+                if (direction==3) len--;
+                if (counter<i && direction==1) len--;
+                counter=0;
+            }
+            counter++;
+
+            if (direction>3) direction=0;
+
+            switch (direction){
+                case 0 : col++; break;
+                case 1 : row++; break;
+                case 2 : col--; break;
+                case 3 : row--; break;
+            }
+
+        }
+        return snailSorted;
+    }
+}
