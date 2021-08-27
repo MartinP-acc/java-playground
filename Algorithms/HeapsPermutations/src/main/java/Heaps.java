be659ea897ecdcde004701191ccb36bd6072d09d
Martin <marcin.paszkowski86@gmail.com>
2021-08-27 17:53:41 +0200

    add Heap's Algorithm

---
+++ Algorithms/HeapsPermutations/src/main/java/Heaps.java
@@ -0,0 +1,31 @@
+import java.util.Arrays;
+
+public class Heaps {
+
+    public static void start(int[] array){
+        int k = array.length;
+        permutation(k,array);
+    }
+
+    private static void swap(int[] array,int a,int b){
+        int temp = array[a];
+        array[a] = array[b];
+        array[b] = temp;
+    }
+
+    private static void permutation(int k,int[] array){
+        if (k==1){
+            Arrays.stream(array).forEach(i -> System.out.print(i+" "));
+            System.out.println();
+        }else {
+            for (int i=0; i<k-1; i++){
+                permutation(k -1, array);
+                if (k%2==0) {
+                    swap(array, i,k-1);
+                } else {
+                    swap(array, 0, k-1);
+                }
+            }
+        }
+    }
+}
---
+++ Algorithms/HeapsPermutations/src/main/java/Main.java
@@ -0,0 +1,7 @@
+public class Main {
+
+    public static void main(String[] args) {
+
+        Heaps.start(new int[]{1,2,3,4,5,6});
+    }
+}
