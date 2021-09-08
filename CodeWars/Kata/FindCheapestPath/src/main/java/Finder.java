a62ec4b81f38a3299611d246463d9bd319b9190f
Martin <marcin.paszkowski86@gmail.com>
2021-09-08 12:20:27 +0200

    add Cheapest path - 4th try (Success!)

--- CodeWars/Kata/FindCheapestPath/src/main/java/Finder.java
+++ CodeWars/Kata/FindCheapestPath/src/main/java/Finder.java
@@ -1,113 +1,83 @@
+import java.awt.*;
 import java.util.*;
-import java.awt.Point;
-
-class Node{
-
-    public int weight;
-    public Node previous;
-    public String direction;
-
-    Node(int row, int col){
-        this.weight = Finder.grid[row][col];
-    }
-
-    Node(int addWeight, Node previous, String direction){
-        setNode(addWeight,previous,direction);
-    }
-
-    public void setNode(int weight, Node previous, String direction){
-        this.weight = weight;
-        this.previous = previous;
-        this.direction = direction;
-    }
-
-    public List<Point> neighbours(Point c){
-        List<Point> n = new ArrayList<>();
-        if (c.x>0) n.add(new Point(c.x-1,c.y));
-        if (c.x<Finder.xLimit) n.add(new Point(c.x+1,c.y));
-        if (c.y>0) n.add(new Point(c.x,c.y-1));
-        if (c.y<Finder.yLimit) n.add(new Point(c.x,c.y+1));
-        return n;
-    }
-}
+import java.util.List;
 
 public class Finder {
 
-    public static int[][] grid;
-    public static int xLimit;
-    public static int yLimit;
-    public static Map<Point,Node> openSet;
-    public static List<Point> closedSet;
-    public static Point start;
-    public static Point end;
-    public static Stack<String> path;
+    private static int[][] grid;
+    private static Map<Point,Integer> weights;
+    private static Map<Point,Point> parents;
+    private static int height;
+    private static int width;
 
-    private static List<String> findPath(){
-
-        path = new Stack<>();
-        closedSet = new ArrayList<>();
-        openSet = new HashMap<>();
-        openSet.put(start, new Node(start.x, start.y));
-
-        while (!openSet.isEmpty()){
-
-            Point current = findLowestWeight(openSet);
-            Node currentNode = openSet.get(current);
-
-            if (current.equals(end)) return reconstructPath(currentNode);
-
-            for (Point neighbor : currentNode.neighbours(current)){
-                if (!closedSet.contains(neighbor)){
-                    int temp = currentNode.weight + grid[neighbor.x][neighbor.y];
-                    String direction = setDirection(current,neighbor);
-                    if (openSet.containsKey(neighbor)){
-                        if (temp < openSet.get(neighbor).weight){
-                            openSet.get(neighbor).setNode(temp,currentNode,direction);
-                        }
-                    } else {
-                        openSet.put(neighbor,new Node(temp,currentNode,direction));
-                    }
-                }
-            }
-            closedSet.add(current);
-            openSet.remove(current);
-        }
-        return new ArrayList<>();
+    private static void init(int[][] t){
+        grid = t;
+        height = t.length;
+        width = t[0].length;
     }
 
-    private static List<String> reconstructPath(Node currentNode) {
-        Node node = currentNode;
-        while (node.previous!=null){
-            path.push(node.direction);
-            node = node.previous;
-        }
-        return path;
+    private static int getCost(Point p){
+        return grid[p.x][p.y];
     }
 
-    private static String setDirection(Point c, Point n) {
-        if (c.x < n.x) return "up";
-        if (c.x > n.x) return "down";
-        if (c.y < n.y) return "left";
-        return "right";
+    private static List<Point> neighbours(Point p){
+        List<Point> pointList = new ArrayList<>();
+        if (p.x>0) pointList.add(new Point(p.x-1,p.y));
+        if (p.y>0) pointList.add(new Point(p.x,p.y-1));
+        if (p.x<height-1) pointList.add(new Point(p.x+1,p.y));
+        if (p.y<width-1) pointList.add(new Point(p.x,p.y+1));
+        return pointList;
     }
 
-    private static Point findLowestWeight(Map<Point,Node> openSet) {
-        Point winner = new Point(-1,-1);
-        for (Map.Entry<Point,Node> spot : openSet.entrySet()){
-            if (winner.x == -1) winner = spot.getKey();
-            else {
-                if (spot.getValue().weight < openSet.get(winner).weight) winner = spot.getKey();
+    private static Point findWithLowestCost(){
+        int minCost = Integer.MAX_VALUE;
+        Point winner = new Point();
+        for (Map.Entry<Point,Integer> pair : weights.entrySet()){
+            if (pair.getValue()<minCost){
+                minCost=pair.getValue();
+                winner=pair.getKey();
             }
         }
         return winner;
     }
 
+    private static List<String> getPath(Point p, Point finish){
+        List<String> path = new ArrayList<>();
+        while (!p.equals(finish)){
+            Point prev = parents.get(p);
+            if (prev.x > p.x) path.add("down");
+            else if (prev.x < p.x) path.add("up");
+            else if (prev.y > p.y) path.add("right");
+            else path.add("left");
+            p = prev;
+        }
+        return path;
+    }
+
     public static List<String> cheapestPath(int[][] t, Point start, Point finish){
-        Finder.start = finish;
-        end = start;
-        grid = t;
-        xLimit = grid.length-1;
-        yLimit = grid[0].length-1;
-        return findPath();
+        init(t);
+
+        Set<Point> visited = new HashSet<>();
+        weights = new HashMap<>();
+        parents = new HashMap<>();
+
+        weights.put(finish,getCost(finish));
+        Point current = finish;
+
+        while (!current.equals(start)){
+            for(Point n : neighbours(current)){
+                if (!visited.contains(n)){
+                    int tempCost = weights.get(current)+getCost(n);
+                    if (!weights.containsKey(n) || tempCost<weights.get(n)){
+                        weights.put(n,tempCost);
+                        parents.put(n,current);
+                    }
+                }
+            }
+            weights.remove(current);
+            visited.add(current);
+            current = findWithLowestCost();
+        }
+        return getPath(current,finish);
     }
 }
