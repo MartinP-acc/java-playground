a15837331b473a62eb40400ef643a7f2d4c6ae1d
Marcin Paszkowski <84535183+MartinP-acc@users.noreply.github.com>
2023-03-07 22:21:36 +0100

    Spring in Action - chapter 3.1.3

---
+++ Spring/taco-cloud/src/main/resources/data.sql
@@ -0,0 +1,16 @@
+delete from Taco_Order_Tacos;
+delete from Taco_Ingredient;
+delete from Taco;
+delete from Taco_Order;
+
+delete from Ingredient;
+insert into Ingredient (id, name, type) values ("FLTO", "wheat", "WRAP");
+insert into Ingredient (id, name, type) values ("COTO", "corn", "WRAP");
+insert into Ingredient (id, name, type) values ("GRBF", "ground beef", "PROTEIN");
+insert into Ingredient (id, name, type) values ("CARN", "piece of meat", "PROTEIN");
+insert into Ingredient (id, name, type) values ("TMTO", "tomato cubes", "VEGGIES");
+insert into Ingredient (id, name, type) values ("LETC", "lettuce", "VEGGIES");
+insert into Ingredient (id, name, type) values ("CHED", "cheddar", "CHEESE");
+insert into Ingredient (id, name, type) values ("JACK", "monterey Jack", "CHEESE");
+insert into Ingredient (id, name, type) values ("SLSA", "spice tomato sauce", "SAUCE");
+insert into Ingredient (id, name, type) values ("SRCR", "cream", "SAUCE");
